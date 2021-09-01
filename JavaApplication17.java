5679b8ea8bc6f6d864a329a8bc535dea14e0499c
baruc <barucmagevski@gmail.com>
2021-09-01 15:33:50 -0300

    add: JavaApplication17.java

---
+++ JavaApplication17.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author Baruc
+ */
+public class JavaApplication17 {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, git");
+    }
+    
+}
